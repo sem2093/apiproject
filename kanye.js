44214adb08f0fdf7f2f6d67de4042856e9013b1d
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-07-03 16:36:09 -0800

    Update kanye.js

--- kanye.js
+++ kanye.js
@@ -1,11 +1,13 @@
-const quoteContainer = document.getElementById('quote-container');
+function fetchQuote() {
+  const quoteContainer = document.getElementById('quote-container');
 
-fetch('https://api.kanye.rest/')
-  .then(response => response.json())
-  .then(data => {
-    const quote = data.quote+" "+"-kanye west";
-    quoteContainer.textContent = quote;
-  })
-  .catch(error => {
-    console.error('Error fetching Kanye quote:', error);
-  });
+  fetch('https://api.kanye.rest/')
+    .then(response => response.json())
+    .then(data => {
+      const quote = data.quote + " - Kanye West";
+      quoteContainer.textContent = quote;
+    })
+    .catch(error => {
+      console.error('Error fetching Kanye quote:', error);
+    });
+}
