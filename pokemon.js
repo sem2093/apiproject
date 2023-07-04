d7d39dd78aeff670a239cccd249eb77c855b1849
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-07-03 17:12:54 -0800

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,1 +1,16 @@
+// Make a request to the PokéAPI
+//update number at end of url to cycle through Pokémon 
+fetch('https://pokeapi.co/api/v2/pokemon/12')
+  .then(response => response.json())
+  .then(data => {
+    // Access the Pokémon information from the response data
+    var pokemonName = data.name;
+    var pokemonAbilities = data.abilities.map(ability => ability.ability.name).join(", ");
 
+    // Display the Pokémon information on the webpage
+    document.getElementById("pokemonNameContainer").textContent = pokemonName;
+    document.getElementById("pokemonAbilitiesContainer").textContent = pokemonAbilities;
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
