323e38c4dd74e10221e2e86a03b9e27254589e00
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-23 09:50:13 -0600

    Update chucknorris.js

--- Chuck Norris API/chucknorris.js
+++ Chuck Norris API/chucknorris.js
@@ -1,1 +1,13 @@
+// Make a request to the Chuck Norris API
+fetch('https://api.chucknorris.io/jokes/random')
+  .then(response => response.json())
+  .then(data => {
+    // Access the joke from the response data
+    var joke = data.value;
 
+    // Display the joke on the webpage
+    document.getElementById("jokeContainer").textContent = joke;
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
