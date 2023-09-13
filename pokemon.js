6f3a107c3b23531c1718f8127527bedc18baed9f
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-09-13 01:11:33 -0600

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,7 +1,14 @@
+// Generate a random number between 1 and 100
+var randomPokemonNumber = Math.floor(Math.random() * 100) + 1;
+
+// Construct the URL with the random number
+var url = `https://pokeapi.co/api/v2/pokemon/${randomPokemonNumber}`;
+
+
 // Make a request to the PokéAPI
 //update number at end of url to cycle through Pokémon 
 function fetchData() {
-fetch('https://pokeapi.co/api/v2/pokemon/12')
+fetch(url)
   .then(response => response.json())
   .then(data => {
     // Access the Pokémon information from the response data
@@ -9,8 +16,8 @@ fetch('https://pokeapi.co/api/v2/pokemon/12')
     var pokemonAbilities = data.abilities.map(ability => ability.ability.name).join(", ");
 
     // Display the Pokémon information on the webpage
-    document.getElementById("pokemonNameContainer").textContent = pokemonName;
-    document.getElementById("pokemonAbilitiesContainer").textContent = pokemonAbilities;
+    document.getElementById("pokemonNameContainer").textContent = "Name: "+ pokemonName;
+    document.getElementById("pokemonAbilitiesContainer").textContent = "Ability: " + pokemonAbilities;
   })
   .catch(error => {
     console.log('Error:', error);
